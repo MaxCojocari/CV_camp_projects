0d0a116e8b9b391b87a627a8c3db0a9ff5fd4d2a
MaxCojocari <92053176+MaxCojocari@users.noreply.github.com>
2021-10-06 21:28:57 +0300

    Add files via upload

---
+++ HW1_CVcamp_var2.py
@@ -0,0 +1,8 @@
+common_year = {1:31, 2:28, 3:31, 4:30, 5:31, 6:30, 7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
+leap_year = {1:31, 2:29, 3:31, 4:30, 5:31, 6:30, 7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
+year = int(input())
+month = int(input())
+if year%4 == 0:
+    print(leap_year[month])
+else:
+    print(common_year[month])
